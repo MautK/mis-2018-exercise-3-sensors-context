fa496cb3852053a74d1acdc02cebfed4f939e7a6
MautK <maud.canisius@gmail.com>
2018-05-04 17:44:26 +0200

    add sensorListeners

--- app/src/main/java/com/example/mis/sensor/MainActivity.java
+++ app/src/main/java/com/example/mis/sensor/MainActivity.java
@@ -1,30 +1,21 @@
 package com.example.mis.sensor;
 
-import android.app.ListActivity;
 import android.content.Context;
-import android.content.res.AssetFileDescriptor;
-import android.content.res.AssetManager;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
-import android.os.AsyncTask;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.view.Display;
+import android.view.Surface;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.example.mis.sensor.FFT;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     //example variables
     private double[] rndAccExamplevalues;
@@ -33,10 +24,13 @@ public class MainActivity extends AppCompatActivity {
     MediaPlayer m;
     private Object view;
     private static final String TAG = "oncreate";
-
     //create an instance of the class
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,29 +38,26 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: Yess helloohoooww");
         setContentView(R.layout.activity_main);
 
-
-
-        //identifying that the sensors are on your device
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-        //check if there is an accelerometer available
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
-            Log.d(TAG, "onCreate: There is an accelerometer!!");
-        } else {
-            Log.d(TAG, "onCreate: ERROR, no accelerometer!!");
-        }
-
-
-
-
         //initiate and fill example array with random values
         //rndAccExamplevalues = new double[64];
         //randomFill(rndAccExamplevalues);
         //new FFTAsynctask(64).execute(rndAccExamplevalues);
 
+        //followed this explaination
+        // https://developer.android.com/guide/topics/sensors/sensors_overview
+        //identifying that the sensors are on your device
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
+        //check if there is an accelerometer available
+        //might be a bit double because we are checking for the same thing in onSensorChanged
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            Log.d(TAG, "onCreate: There is an accelerometer!!");
+        } else {
+            Log.d(TAG, "onCreate: ERROR, no accelerometer!!");
+        }
     }
+
 
 
 
@@ -133,5 +124,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //followed this example
+    // https://developer.android.com/reference/android/hardware/SensorManager
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+
+    //followed this example
+    //  https://developer.android.com/reference/android/hardware/SensorManager
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+    //here is where you read the current sensor values
+        // and update views that are displaying the sensor information
+     // do as little as possible here to not block it!!
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
+
